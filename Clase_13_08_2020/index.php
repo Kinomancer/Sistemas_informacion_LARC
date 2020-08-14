8929deeff3bcd9aba467c8256047abcc31929504
Kinomancer <luisramos99@aragon.unam.mx>
2020-08-13 20:06:04 -0500

    Tarea 5 Luis Angel Ramos Cruz

---
+++ Clase_13_08_2020/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Ejercicio 1</title>
+</head>
+
+<body class="greenbg">
+    <div class="lettersize whiteletter title">
+        Ejemplo 1, con una tabla
+    </div>
+    <div>
+        <table class="lettersize blackletter grayborder divsize">
+            <tr>
+                <td class="margin">
+                    <div class="bluebg blackborder">
+                        Fila 1
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td class="margin">
+                    <div class="purplebg blackborder righttext">
+                        Fila 2
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td class="margin">
+                    <div class="pinkbg blackborder">
+                        Fila 3
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <!--Fin de la tabla1-->
+
+    <div class="lettersize whiteletter title">
+        Ejemplo 2, con tres tablas
+    </div>
+    <table class="divsize dmargin grayborder">
+        <td>
+            <div class="lettersize blackletter blackborder abg bmargin">
+                Fila 1
+            </div>
+        </td>
+    </table>
+
+    <table class="divsize dmargin grayborderm">
+        <td>
+            <div class="lettersize blackletter blackborder bbg righttext bmargin">
+                Fila 2
+            </div>
+        </td>
+    </table>
+
+    <table class="divsize dmargin grayborderf">
+        <td>
+            <div class="lettersize blackletter blackborder cbg bmargin">
+                Fila 3
+            </div>
+        </td>
+    </table>
+
+    <!--Fin de la tabla 2-->
+
+    <div class=" lettersize whiteletter title">
+        Ejemplo 3, con tres divs
+    </div>
+
+    <div class="lettersize blackletter xbg">
+        div 1
+    </div>
+    <div class="lettersize blackletter righttext ybg">
+        div 2
+    </div>
+    <div class="lettersize blackletter zbg">
+        div 3
+    </div>
+
+    <!--Fin de la tabla 3-->
+
+</body>
+
+</html>
